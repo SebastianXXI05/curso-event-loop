cd8e44df87d2c7fb4e750f932a86861b847842b2
Juan Sebastian <sebastianmorales05col@gmail,com>
2022-09-30 15:46:21 -0500

    bloqueo y renderizado, event loop y renderizado

---
+++ basic/blocking.js
@@ -0,0 +1,33 @@
+const btnClick = document.getElementById('btn-click')
+const numClicks = document.getElementById('num-clicks')
+const btnSlow = document.getElementById('btn-slow')
+
+let clicks = 0;
+btnClick.onclick = () => {
+  numClicks.textContent = `Number of clicks: ${++clicks}`
+}
+
+const compute = (num) => {
+  const limit = 7000
+  for (let i = 0; i < limit; i++) {
+    for (let j = 0; j < limit; j++) {
+      num **=2
+      num = Math.sqrt(num)
+    }
+  }
+  return num
+}
+
+/* btnSlow.onclick = () => {
+  console.time()
+  console.log(compute(4))
+  console.timeEnd()
+} */
+
+btnSlow.onclick = () => {
+  const nums = [1.5, 2.5, 3.5, 4.5]
+  for (let n of nums) {
+    // console.log(compute(n))
+    setTimeout(() => console.log(compute(n)), 0)
+  }
+}
